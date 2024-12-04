a5ce4279ffdc396742d839468430f79da375e78e
Freek Bes <freek.bes@codam.nl>
2024-12-04 14:49:56 +0100

    add paywall warning before printing

--- js/print.js
+++ js/print.js
@@ -4,6 +4,26 @@ function getArticle() {
 	return document.querySelector('article');
 }
 
+function checkForPaywall() {
+	const paywall = document.querySelector('.paywall');
+	return (paywall !== null);
+}
+
+function addPaywallNotice() {
+	console.log('[Substack Printing Styles] Adding paywall notice to the beginning of the article');
+	const article = getArticle();
+	const paywallNotice = document.createElement('div');
+	paywallNotice.classList.add('paywall-notice');
+	paywallNotice.style.color = 'white';
+	paywallNotice.style.backgroundColor = 'darkred';
+	paywallNotice.style.fontWeight = 'bold';
+	paywallNotice.style.padding = '1em';
+	paywallNotice.style.marginBottom = '1em';
+	paywallNotice.textContent = 'This article is behind a paywall and may not print properly. Either log in to Substack or click the "View in browser" link in the newsletter e-mail to view and print the article.';
+	article.insertBefore(paywallNotice, article.firstChild);
+	console.log(paywallNotice);
+}
+
 function copyNewsletterTitleToPrint() {
 	console.log('[Substack Printing Styles] Copying newsletter title to print');
 	const titleElem = document.querySelector('.topbar-content .navbar-title');
@@ -37,12 +57,22 @@ function copyArticleCopyrightToPrint() {
 	console.log('[Substack Printing Styles] Footer copyright blurb:', footerCopyrightBlurb);
 	const article = getArticle();
 	const printPostFooter = article.querySelector('.print-post-footer.substack-printing-only');
+	if (!printPostFooter) {
+		console.warn('[Substack Printing Styles] No print footer found, cannot copy article copyright!');
+		return;
+	}
 	// Copy the copyright to the print footer by cloning the element
 	const copyrightClone = footerCopyrightBlurb.cloneNode(true);
 	printPostFooter.appendChild(copyrightClone);
 	console.log('[Substack Printing Styles] Copyright cloned:', copyrightClone);
 }
 
-createPrintPostFooter();
-copyNewsletterTitleToPrint();
-copyArticleCopyrightToPrint();
+if (checkForPaywall()) {
+	console.warn('[Substack Printing Styles] Paywall detected! This article wouldn\'t print properly.');
+	addPaywallNotice();
+}
+else {
+	createPrintPostFooter();
+	copyNewsletterTitleToPrint();
+	copyArticleCopyrightToPrint();
+}
